9a3d5cf9dddc7a643ff5f32ccd6e5ca8ddd9cd9b
Junior <guitar.sound12@gmail.com>
2023-01-03 13:46:21 -0500

    correcciones

--- components/form.php
+++ components/form.php
@@ -5,32 +5,32 @@
     <form class="secForm__form" action="https://formsubmit.co/pecrash12@gmail.com" method="POST">
         <div class="secForm__form-input">
             <span class="secForm__form-input-icon"><img src="../assets/form_icons/user.svg" alt=""></span>
-            <input type="text" placeholder="YOUR NAME">
+            <input type="text" placeholder="YOUR NAME" name="name" required>
         </div>
 
         <div class="secForm__form-input">
             <span class="secForm__form-input-icon"><img src="../assets/form_icons/phone.svg" alt=""></span>
-            <input type="text" placeholder="PHONE NUMBER">
+            <input type="text" placeholder="PHONE NUMBER" name="number" required>
         </div>
 
         <div class="secForm__form-input">
             <span class="secForm__form-input-icon"><img src="../assets/form_icons/scissors.svg" alt=""></span>
-            <input type="text" placeholder="SERVICE">
+            <input type="text" placeholder="SERVICE" name="service" >
         </div>
 
         <div class="secForm__form-input">
             <span class="secForm__form-input-icon"><img src="../assets/form_icons/calendar.svg" alt=""></span>
-            <input type="date" placeholder="DATE">
+            <input type="date" placeholder="DATE" name="date">
         </div>
 
         <div class="secForm__form-input">
             <span class="secForm__form-input-icon"><img src="../assets/form_icons/watch.svg" alt=""></span>
-            <input type="time" placeholder="TIME">
+            <input type="time" placeholder="TIME" name="time">
         </div>
 
         <div class="secForm__form-input">
             <span class="secForm__form-input-icon-area"><img src="../assets/form_icons/message.svg" alt=""></span>
-            <textarea name="" id="" placeholder="YOUR MESSAGE"></textarea>
+            <textarea name="" id="" placeholder="YOUR MESSAGE" name="subject" required></textarea>
         </div>
         <div class="secForm__form-submit">
             <input class="secForm__form-submit-btn" type="submit" value="APPOINTMENT">
